6b70a6fdf0e5744119cb58239af95f1da651c520
aberg07 <140882154+aberg07@users.noreply.github.com>
2024-02-22 11:59:52 +0900

    Initial commit

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var cors = require('cors');
-require('dotenv').config()
+const multer = require('multer');
+const upload = multer({ dest: './uploads'});
+require('dotenv').config();
 
 var app = express();
 
@@ -11,7 +13,14 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  res.json({
+    //req.file.filename will automatically rename uploaded files, so req.file.originalname is used instead
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  });
+})
 
 
 const port = process.env.PORT || 3000;
